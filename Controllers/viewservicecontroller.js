b6abed6623fb5d6c542cc51a539ac42f69c59d14
mennaallahmostafa <mennaallah.osman@student.guc.edu.eg>
2017-04-07 00:03:00 +0200

    services final

--- Controllers/viewservicecontroller.js
+++ Controllers/viewservicecontroller.js
@@ -1,27 +1,28 @@
-let work = require ('../model/work');
+let work = require ('../Models/work');
 let workController = {
 	getallservices:function(req,res){
 
-	work.find(function(err,projects){
+	work.find(function(err,Work){
 	if (err)
 	res.send(err.message);
 	else 
-	res.render('home',{projects});
+	res.render('home',{Work});
 	})
 	},
 
 getallcompanies:function(req,res){
 var companyname = req.body.companynamerequested;
-work.find ({Username:companyname}function(err,projects))
+work.find ({Username:companyname}function(err,Work))
 {if (err)
 	res.send(err.message);
 	else 
-	res.render('home',{projects});
+	res.render('home',{Work});
 	})
 	
 }
 }
  
 }
+
 
 
