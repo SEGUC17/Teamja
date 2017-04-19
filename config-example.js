ae00d378e8e2e92ddc1554f6ac5636c7d3ee656f
Omar Raslan <you@example.com>
2017-04-19 23:28:29 +0200

    Payment

---
+++ config-example.js
@@ -0,0 +1,5 @@
+// Get your keys here:
+// https://dashboard.stripe.com/account/apikeys
+module.exports = {
+  secret: 'sk_test_69qIQ4etAnofSMXMYO6cgwHh',
+  client: 'pk_test_HumzHShuCgWvtWLVpySgRmyZ'};
