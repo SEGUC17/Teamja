a713fc435d0efeae660de65cd5716a0b0205fc1a
injyamr <injyamr123@gmail.com>
2017-04-25 01:56:55 +0200

    Delete userprofile-controller.js

--- public/controllers/userprofile-controller.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    angular.module('startup')
-.controller('userprofile', ['$scope','$rootScope', '$state', '$http', function($scope,$rootScope, $state, $http){
-  
-  $scope.user=$rootScope.name;
-        
-    }]);
-}());
